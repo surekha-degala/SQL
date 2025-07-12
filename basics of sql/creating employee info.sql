dee939ae5aa9ecfd79482d3cb570768e5d2f14da
surekha-degala <surekhadegala22@gmail.com>
2025-07-12 16:14:21 +0530

    Create creating employee info.sql

---
+++ basics of sql/creating employee info.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employee_info(
+  id INT PRIMARY KEY, 
+  name VARCHAR(100), 
+ salary INT 
+);
+  INSERT INTO employee_info (id, name, salary)
+  VALUES (1, suku, 60,000), 
+(2, swetha , 80,000), 
+(3, surekha, 100,000)
+);
+
+SELECT * FROM employee_info;
